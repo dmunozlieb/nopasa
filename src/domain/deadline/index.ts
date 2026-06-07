0a1e5c0ef8a70efa5f466fd814c801427006f6f2
dmunozlieb <danielmunozliebana@gmail.com>
2026-06-07 20:33:57 +0200

    feat(domain): add public barrel for the deadline domain

---
+++ src/domain/deadline/index.ts
@@ -0,0 +1,25 @@
+export {
+  deadlineSchema,
+  DeadlineType,
+  DeadlineStatus,
+  type Deadline,
+} from './deadline.schema';
+export {
+  createDeadline,
+  type CreateDeadlineInput,
+  type CreateDeadlineDeps,
+  type IdGenerator,
+  type Clock,
+} from './deadline.factory';
+export {
+  daysRemaining,
+  urgencyLevel,
+  URGENT_MAX_DAYS,
+  UPCOMING_MAX_DAYS,
+  type UrgencyLevel,
+} from './urgency';
+export {
+  groupAndSort,
+  type DeadlineGroup,
+  type GroupedDeadlines,
+} from './grouping';
